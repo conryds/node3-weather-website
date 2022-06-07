f77e50e4b515f6eaa42ec604e12ddce5cba58152
Conryd Salvesen <conryd@provencfo.com>
2022-06-06 19:46:25 -0600

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('postman-request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=693a2321d98b42074f8f877ef8a5adb7&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather sercvice!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees!')
+        }
+    })
+
+}
+
+
+
+module.exports = forecast
